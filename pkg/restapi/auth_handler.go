aac9a909675a5cf3f24475a4ab508510697e1e59
Igor Baliuk <baliuk.igor@gmail.com>
2022-04-23 22:29:20 +0400

    Issue accses tokens

--- pkg/restapi/auth_handler.go
+++ pkg/restapi/auth_handler.go
@@ -3,10 +3,12 @@ package restapi
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/lodthe/rest-auth-example/internal/auth"
 	"github.com/lodthe/rest-auth-example/internal/muser"
+	"github.com/pkg/errors"
 	zlog "github.com/rs/zerolog/log"
 )
 
@@ -24,6 +26,7 @@ func newAuthHandler(authService *auth.Service, userRepo muser.Repository) *authH
 
 func (h *authHandler) handle(r chi.Router) {
 	r.Post("/auth/register", h.register)
+	r.Post("/auth/issue-access-token", h.issueAccessToken)
 }
 
 type RegisterInput struct {
@@ -82,5 +85,42 @@ func (h *authHandler) register(w http.ResponseWriter, r *http.Request) {
 
 	writeResult(w, RegisterOutput{
 		RefreshToken: token,
+	})
+}
+
+type IssueAccessToken struct {
+	AccessToken string `json:"access_token"`
+}
+
+func (h *authHandler) issueAccessToken(w http.ResponseWriter, r *http.Request) {
+	header := r.Header.Get("Authorization")
+	refreshToken, err := h.auth.FetchToken(strings.TrimPrefix(header, "Bearer "))
+	if err != nil {
+		if errors.Is(err, auth.ErrUnauthorized) || errors.Is(err, auth.ErrInvalidToken) {
+			writeError(w, "invalid token", http.StatusBadRequest)
+		} else {
+			zlog.Error().Err(err).Msg("token fetch failed")
+			writeError(w, "internal error", http.StatusInternalServerError)
+		}
+
+		return
+	}
+	if refreshToken.IsExpired() {
+		writeError(w, "token has expired", http.StatusBadRequest)
+		return
+	}
+
+	_, accessToken, err := h.auth.IssueAccessToken(refreshToken.ID, refreshToken.UserID)
+	if err != nil {
+		zlog.Error().Err(err).Interface("refresh_token", refreshToken).Msg("failed to issue an access token")
+		writeError(w, "token cannot be issued", http.StatusInternalServerError)
+
+		return
+	}
+
+	zlog.Info().Str("user_id", refreshToken.UserID.String()).Msg("a new access token has been issued")
+
+	writeResult(w, IssueAccessToken{
+		AccessToken: accessToken,
 	})
 }
